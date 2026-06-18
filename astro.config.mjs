3db044d3df82629271629a3c6a9de0ea4414fcdc
yassine0malal <malalyassin6@gmail.com>
2026-06-18 18:02:27 +0100

    after fix the problem of the wrong path of the name and the base folders

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://username.github.io',
-  base: '/repo-name',
+  site: 'https://yassine0malal.github.io',
+  base: '/circles',
   vite: {
     plugins: [tailwindcss()],
     css: {
